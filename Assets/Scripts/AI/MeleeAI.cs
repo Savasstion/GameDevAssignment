6feca37f45e0d7413a53ed353383b99ffa95752d
Savasstion <savasstion1108@gmail.com>
2023-08-27 20:47:19 +0800

    file name change

--- Assets/Scripts/AI/MeleeAI.cs
+++ Assets/Scripts/AI/MeleeAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class MeleeAI : MonoBehaviour
 {
     [SerializeField]
     private List<Detector> detectors;
